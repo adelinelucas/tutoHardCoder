66b0d8cbe865bb1b829a03d51e57682e3cd2498e
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2022-08-02 16:48:46 +0200

    ajout proxy et build

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
+
+// récupération des fichiers build de l'app react 
+app.use(express.static('client/build'));
 app.get('/api/tuto', (req, res)=>{
     res.send({
         msg: "Bonjour bienvenue pour cette démo express / react"
     })
 });
+
+app.get('/*', (req, res)=>{
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, ()=>{
